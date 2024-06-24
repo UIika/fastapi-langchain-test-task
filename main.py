2a05000b5a0614827d196a24d6ef3bcab7464395
1lys <1lys.rynd@gmail.com>
2024-06-25 00:49:07 +0300

    comments

--- main.py
+++ main.py
@@ -3,23 +3,25 @@ from pydantic import BaseModel
 import uvicorn
 from langchain_huggingface import HuggingFacePipeline
 
-
+# language model creation
 llm = HuggingFacePipeline.from_model_id(
     model_id="facebook/bart-large-cnn",
     task="summarization",
 )
 
+# pydantic schema for validating user text input
 class TextSchema(BaseModel):
     text: str
 
+
 app = FastAPI(title='FastAPI Langchain test task')
 
-
+# endpoint for summarizing
 @app.post("/summarize", tags=['summarize'])
 async def summarize(text_schema: TextSchema):
     summary = llm.invoke(text_schema.text)
     return {"summary": summary}
 
-
+# hosting uvicorn server if this module is executed
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
